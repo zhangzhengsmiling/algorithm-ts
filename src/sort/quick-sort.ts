5c5926eb3266bfdd43dd951167c6a87b42d28f61
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-02-04 10:12:42 +0800

    perf(quick-sort): optimize, random quick sort
    - 随机快排算法优化

--- src/sort/quick-sort.ts
+++ src/sort/quick-sort.ts
@@ -1,22 +1,42 @@
+/**
+ * 生成随机值
+ * 辅助随机快排算法
+ * @param start range of random number
+ * @param end 
+ */
+const random = (start: number, end: number) => {
+  return Math.floor(start + Math.random() * (end - start + 1));
+}
+
+/**
+ * 交换数组中的两个元素位置
+ * @param array 数组引用值
+ * @param index1 下标
+ * @param index2 下标
+ */
+const swapArray = (array: number[], index1: number, index2: number) => {
+  const temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+}
+
 const partition = (array: number[], begin: number, end: number) => {
   if(array.length === 0) return begin;
-  const target = array[begin];
-  // console.log(array)
+  // 随机生成一个下标作为主元，与begin值交换，随机化快排过程
+  const idx_pivot = random(begin, end);
+  swapArray(array, idx_pivot, begin);
+  const pivot = array[begin];
   let ptr_left = begin + 1;
   let ptr_right = end;
   while(ptr_left < ptr_right) {
-    while(ptr_left < ptr_right && array[ptr_left] < target) ptr_left++;
-    while(ptr_right > ptr_left && array[ptr_right] > target) ptr_right--;
-    const temp = array[ptr_left];
-    array[ptr_left] = array[ptr_right];
-    array[ptr_right] = temp;
+    while(ptr_left < ptr_right && array[ptr_left] < pivot) ptr_left++;
+    while(ptr_right > ptr_left && array[ptr_right] > pivot) ptr_right--;
+    swapArray(array, ptr_left, ptr_right)
   }
-  if(target < array[ptr_left]) {
+  if(pivot < array[ptr_left]) {
     ptr_left--;
   }
-  const temp = array[begin];
-  array[begin] = array[ptr_left];
-  array[ptr_left] = temp;
+  swapArray(array, begin, ptr_left);
   return ptr_left;
 }
 
