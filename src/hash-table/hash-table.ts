357ad82e88c44b690b1ad875dacf9dcb551c2799
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-02-23 00:14:17 +0800

    feat(hash-table): add data structure, hash table
    - 散列表，数据结构

---
+++ src/hash-table/hash-table.ts
@@ -0,0 +1,16 @@
+
+class ChainedHashTable {
+  private hashTable: any[] = [];
+  constructor(length: number) {
+    this.hashTable = new Array(length);
+  }
+  search(key: string) {
+
+  }
+  insert(key: string, value: any) {
+
+  }
+  remove(key: string) {
+    
+  }
+}
