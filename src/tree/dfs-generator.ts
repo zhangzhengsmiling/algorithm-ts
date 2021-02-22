c97abb8dd3937e9fb39b128137d90c8fe53af6ab
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-02-22 10:56:53 +0800

    perf(dfs-generator): dfs by using stack, replacing recursion version
    - 深度优先遍历，堆栈实现代替递归

--- src/tree/dfs-generator.ts
+++ src/tree/dfs-generator.ts
@@ -1,4 +1,3 @@
-
 const dfsGenerator = <T>(dfsKey: keyof T) => {
   const dfs = (tree: T | null, callback?: (v: T | null) => void) => {
     if(tree === null || tree === undefined) return;
@@ -12,7 +11,24 @@ const dfsGenerator = <T>(dfsKey: keyof T) => {
       })
     }
   }
-  return dfs;
+  const dfsByStack = (tree: T | null, callback?: (v: T | null) => void) => {
+    if(tree === null || tree === undefined) return;
+    const stack: T[] = [];
+    stack.push(tree);
+    while(stack.length !== 0) {
+      const temp = stack.pop() as T;
+      const children = temp[dfsKey];
+      if(typeof callback === 'function') {
+        callback(temp);
+      }
+      if(children instanceof Array && children.length > 0) {
+        for(let i = children.length - 1; i >= 0; i--) {
+          stack.push(children[i]);
+        }
+      }
+    }
+  }
+  return dfsByStack;
 }
 
 export default dfsGenerator;
